ab276e8dc3d7e8a4be77db5eba6263ff305004fa
Riaz Munshi <riaz.2012@gmail.com>
2026-08-13 15:37:40 -0700

    Step 45: allocate_count_matrix

--- model.py
+++ model.py
@@ -359,8 +359,13 @@ def get_batch(data, block_size, batch_size, rng):
     Y = stack_y_batch(data, offsets, block_size)
     return (X, Y)
 
-# Step 45 - allocate_count_matrix (not yet solved)
-# TODO: implement
+# Step 45 - allocate_count_matrix
+import numpy as np
+
+def allocate_count_matrix(vocab_size):
+    """Allocate a (V, V) integer zero matrix for bigram counts."""
+    # TODO: return a (vocab_size, vocab_size) integer array of zeros.
+    return np.zeros((vocab_size, vocab_size), dtype=np.int64)
 
 # Step 46 - loop_fill_counts (not yet solved)
 # TODO: implement
